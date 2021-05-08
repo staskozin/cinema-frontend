883669bd1b344e686cccd497aa8eb07f6d996e0b
Stanislav Kozin <stas@staskozin.ru>
2021-05-08 19:36:45 +0300

    Доверстал список сеансов для мобильных устройств

--- src/components/Page/MoviePage/ShowtimeList.jsx
+++ src/components/Page/MoviePage/ShowtimeList.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 import s from './ShowtimeList.scss';
 
@@ -7,21 +8,30 @@ import { getDateWithWeekday } from '../../../lib/dates';
 function renderShowtimeList(showtimeList) {
   const grouped = groupShowtimes(showtimeList);
   const days = [];
+  const today = new Date();
   for (const day in grouped) {
     days.push(
-      <div key={day}>
-        <h3 className={s.dayheader}>{day}</h3>
-        {
-          grouped[day].map(sh => {
-            const date = new Date(sh.showtime_date);
-            return (
-              <div key={sh.showtime_id}>
-                <p>{`${date.getHours()}:${date.getMinutes()}`}</p>
-                <p>{sh.price}₽</p>
-              </div>
-            )
-          })
-        }
+      <div className={s.day} key={day}>
+        <h3 className={s.day__header}>{day}</h3>
+        <div className={s.day__times}>
+          {
+            grouped[day].map(sh => {
+              const date = new Date(sh.showtime_date);
+              return (
+                today < date ?
+                  <Link to={`/showtime/${sh.showtime_id}`} className={`${s.day__showtime} ${s.day__showtime_link}`} key={sh.showtime_id}>
+                    <span className={s.day__time}>{`${date.getHours()}:${date.getMinutes()}`}</span><br />
+                    <span className={s.day__price}>{sh.price} ₽</span>
+                  </Link>
+                  :
+                  <div className={s.day__showtime} key={sh.showtime_id}>
+                    <span className={s.day__time}>{`${date.getHours()}:${date.getMinutes()}`}</span><br />
+                    <span className={s.day__price}>{sh.price} ₽</span>
+                  </div>
+              )
+            })
+          }
+        </div>
       </div>
     );
   }
@@ -51,9 +61,9 @@ export default function MoviePage(props) {
   }, []);
 
   return (
-    <>
+    <div className={s.wrap}>
       <h2 className={s.header}>Сеансы</h2>
       {renderShowtimeList(showtime)}
-    </>
+    </div>
   );
 }
