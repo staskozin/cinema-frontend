f84556377b3a10c3f7207f4764fc4d99f2faf89d
Stanislav Kozin <stas@staskozin.ru>
2021-05-10 22:50:50 +0300

    Добавил запрос к API на страницу сеанса

--- src/components/Page/ShowtimePage/ShowtimePage.jsx
+++ src/components/Page/ShowtimePage/ShowtimePage.jsx
@@ -1,7 +1,19 @@
 import React, { useState, useEffect } from 'react';
 
-export default function MoviePage(props) {
+export default function ShowtimePage(props) {
+  const [sh, setShowtime] = useState({});
+
+  useEffect(() => {
+    fetch(`https://cinema-api.staskozin.ru/showtime/${props.match.params.showtime_id}`)
+      .then(r => r.json())
+      .then(r => setShowtime(r))
+      .then(r => console.log(sh))
+  }, []);
+
   return (
-    <h1>Сеанс {props.match.params.showtime_id}</h1>
+    <>
+      <h1>Сеанс {props.match.params.showtime_id}</h1>
+      <p>{sh.showtime_date}</p>
+    </>
   );
 }
